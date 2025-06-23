06ab6e12eb5ac093ad45ca069d243ec0872466d8
Balogun Al-ameen <alameenbalogun61@example.com>
2025-06-23 19:39:41 +0100

    making a post route handler

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
@@ -24,6 +26,30 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+
+  //   console.log(newTour);
+  //   console.log(req.body);
 });
 
 //PORT

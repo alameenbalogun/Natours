b6ba992a559c948afedeeff7a26ab50f88ee5ce3
Balogun Al-ameen <alameenbalogun61@example.com>
2025-06-26 15:42:13 +0100

    modelling the tour

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -131,9 +131,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'sucess',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'sucess',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };

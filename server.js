5704cae0a768bd816db7dea35b198e23c82efd9f
Balogun Al-ameen <alameenbalogun61@example.com>
2025-06-26 14:23:59 +0100

    a dotenv file creation

--- server.js
+++ server.js
@@ -9,42 +9,14 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 // console.log(DB);
+
+//connecting database
 mongoose.connect(DB).then((con) => {
   // console.log(con.connection);
   console.log('Database connected successfully');
 });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-});
 
-const Tour = mongoose.model('Tour', tourSchema);
-
-const tourTest = new Tour({
-  name: 'The park Camper',
-  price: 200,
-});
-
-tourTest
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('Error', err);
-  });
 
 const port = 8000;
 app.listen(port, () => {
